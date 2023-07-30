9016ef7b4dec4126a3860b841c74e8acebee4316
PRESSO <caff1nepill@gmail.com>
2023-07-30 16:31:36 +0900

    Feat: Change default font to Pretendard Variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,21 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Head from '@/app/components/Head'
-
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css';
+import type { Metadata } from 'next';
+import Head from '@/app/components/Head';
 
 export const metadata: Metadata = {
 	title: '교움',
-	description: '교통지원 도움이'
-}
+	description: '교통지원 도움이',
+};
 
 export default function RootLayout({
-	children
+	children,
 }: {
-	children: React.ReactNode
+	children: React.ReactNode;
 }) {
 	return (
-		<html lang="ko">
+		<html lang='ko'>
 			<Head />
-			<body className={inter.className}>{children}</body>
+			<body>{children}</body>
 		</html>
-	)
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		fontFamily: {
+			sans: [
+				'Pretendard Variable',
+				'Pretendard',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'system-ui',
+				'Roboto',
+				'Helvetica Neue',
+				'Segoe UI',
+				'Apple SD Gothic Neo',
+				'Noto Sans KR',
+				'Malgun Gothic',
+				'Apple Color Emoji',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'sans-serif',
+			],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
