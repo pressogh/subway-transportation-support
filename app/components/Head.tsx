443baaef1544b66a33ba9058fe3c4f91bd38ba7e
PRESSO <caff1nepill@gmail.com>
2023-07-30 16:30:51 +0900

    Feat: Change Icon

--- app/components/Head.tsx
+++ app/components/Head.tsx
@@ -1,60 +1,66 @@
 const Head = () => {
 	return (
 		<head>
-			<link rel="manifest" href="/manifest.json" />
+			<link rel='manifest' href='/manifest.json' />
 			<link
-				href="/app-icons/favicon-16x16.png"
-				rel="icon"
-				type="image/png"
-				sizes="16x16"
+				href='/app-icons/favicon-16x16.png'
+				rel='icon'
+				type='image/png'
+				sizes='16x16'
 			/>
 			<link
-				href="/app-icons/favicon-32x32.png"
-				rel="icon"
-				type="image/png"
-				sizes="32x32"
+				href='/app-icons/favicon-32x32.png'
+				rel='icon'
+				type='image/png'
+				sizes='32x32'
 			/>
 			<link
-				rel="apple-touch-icon"
+				href='/app-icons/favicon-96x96.png'
+				rel='icon'
+				type='image/png'
+				sizes='96x96'
+			/>
+			<link
+				rel='apple-touch-icon'
 				sizes={'72x72'}
-				href="/app-icons/icon-72x72.png"
+				href='/app-icons/icon-72x72.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'96x96'}
-				href="/app-icons/icon-96x96.png"
+				href='/app-icons/icon-96x96.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'128x128'}
-				href="/app-icons/icon-128x128.png"
+				href='/app-icons/icon-128x128.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'144x144'}
-				href="/app-icons/icon-144x144.png"
+				href='/app-icons/icon-144x144.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'152x152'}
-				href="/app-icons/icon-152x152.png"
+				href='/app-icons/icon-152x152.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'192x192'}
-				href="/app-icons/icon-192x192.png"
+				href='/app-icons/icon-192x192.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'384x384'}
-				href="/app-icons/icon-384x384.png"
+				href='/app-icons/icon-384x384.png'
 			/>
 			<link
-				rel="apple-touch-icon"
+				rel='apple-touch-icon'
 				sizes={'512x512'}
-				href="/app-icons/icon-512x512.png"
+				href='/app-icons/icon-512x512.png'
 			/>
-			<meta name="msapplication-TileColor" content="#FF98BA" />
+			<meta name='msapplication-TileColor' content='#FF98BA' />
 		</head>
 	);
 };
